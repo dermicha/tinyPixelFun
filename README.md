02cbe7fd83917754aee30257c7e5c20cf5822516
derMicha <dermicha@ubirch.com>
2019-11-04 17:49:17 +0100

    added more source code, very early version of instructions

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # tinyPixelFun
 Simple ATTiny85 Neopixel Example
+
+# Bauteile
+
+# Basis Set
+
+* ATtiny85 20MHz
+* 8 Pin IC Sockel
+* WS2812b RGB LED Stripe (3 Kabel)
+* 3x AA Batteriefach
+* 3x AA Batterien
+* 1x 10uF Kondensator
+* 1x 220 Ohm Vorwiderstand
+* 1x 5x5cm Streifenraster Platine 
+* 1x WS2812b Anschlussklemme mit Kabel (3 Kabel)
+* Kosten ca. 5-6 Eur
+
+## Option 1
+* 1x 220 Ohm Vorwiderstand
+* 1x LED (bel. Farbe)
+* Kosten ca. 0,30 Eur
+
+## Option 2
+* 1x Taster
+* 1x 10k Pullup Widerstand 
+* Kosten ca. 0,30 Eur
+
+## Bauanteilung (work in progress)
+
+Hier findest Du die Anleitung zum Aufbau der Schaltung:
+* [Bauanleitung](Bauanteilung/CMS-CoderDojo.html)
--- neopixelExamples/Strandtest/Strandtest.ino
+++ neopixelExamples/Strandtest/Strandtest.ino
@@ -49,17 +49,17 @@ void setup() {
 
 void loop() {
   // Fill along the length of the strip in various colors...
-  colorWipe(strip.Color(255,   0,   0), 50); // Red
-  colorWipe(strip.Color(  0, 255,   0), 50); // Green
-  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
+//  colorWipe(strip.Color(255,   0,   0), 50); // Red
+//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
+//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
 
   // Do a theater marquee effect in various colors...
-  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
-  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
+//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
+//  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
+//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
 
   rainbow(10);             // Flowing rainbow cycle along the whole strip
-  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
 
 
